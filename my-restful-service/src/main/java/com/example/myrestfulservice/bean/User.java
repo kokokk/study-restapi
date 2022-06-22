daca56d2c60ca651e5e92f09a67fead4a2a5326d
kokokk <portaltopr@gmail.com>
2022-06-22 10:46:55 +0900

    add swagger and actuator

--- my-restful-service/src/main/java/com/example/myrestfulservice/bean/User.java
+++ my-restful-service/src/main/java/com/example/myrestfulservice/bean/User.java
@@ -3,6 +3,8 @@ package com.example.myrestfulservice.bean;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,10 +19,12 @@ import java.util.Date;
 @NoArgsConstructor
 @Builder
 //@JsonIgnoreProperties(value = {"password", "ssn"})
-@JsonFilter("UserInfo")
+//@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보")
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
     @Past
     private Date joinDate;
--- my-restful-service/src/main/java/com/example/myrestfulservice/controller/UserController.java
+++ my-restful-service/src/main/java/com/example/myrestfulservice/controller/UserController.java
@@ -3,14 +3,24 @@ package com.example.myrestfulservice.controller;
 import com.example.myrestfulservice.bean.User;
 import com.example.myrestfulservice.exception.UserNotFoundException;
 import com.example.myrestfulservice.service.UserDaoService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/users")
@@ -24,20 +34,40 @@ public class UserController {
     }
 
     @GetMapping("")
-    public List<User> retrieveAllUsers() {
-        return service.findAll();
+    @ApiOperation(value = "사용자 목록 조회", notes = "등록된 전체 사용자의 목록을 조회합니다.")
+    public ResponseEntity<CollectionModel<EntityModel<User>>> retrieveAllUsers() {
+        List<EntityModel<User>> result = new ArrayList<>();
+        List<User> users = service.findAll();
+
+        for (User user : users) {
+            EntityModel entityModel = EntityModel.of(user);
+//            entityModel.add(linkTo(methodOn(this.getClass()).retrieveAllUsers()).withSelfRel());
+            entityModel.add(linkTo(methodOn(this.getClass()).retrieveUserById(user.getId())).withRel("view"));
+
+            result.add(entityModel);
+        }
+        return ResponseEntity.ok(CollectionModel.of(result, linkTo(methodOn(this.getClass()).retrieveAllUsers()).withSelfRel()));
     }
 
     // GET http://localhost:8080/users/10000
     @GetMapping("/{id}")
-    public User retrieveUserById(@PathVariable(value = "id") int id) {
+    @ApiOperation(value = "사용자 상세 정보 조회", notes = "사용자의 ID를 이용하여 상세정보 보기")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "사용자 ID", required = true, paramType = "path")
+    })
+    public ResponseEntity retrieveUserById(@PathVariable(value = "id") int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException(id);
         }
 
-        return user;
+        EntityModel entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linkTo.withRel("all-users"));
+
+        return ResponseEntity.ok(entityModel);
     }
 
     @PostMapping("")
